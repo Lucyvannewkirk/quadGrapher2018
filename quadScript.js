780e8760b1edcb5f6a73448f08c4ff0843562ec9
Lucyvannewkirk <35277779+Lucyvannewkirk@users.noreply.github.com>
2018-02-01 13:18:25 -0800

    Up dated

--- quadScript.js
+++ quadScript.js
@@ -1,5 +1,5 @@
 //Javascript for drawing and calculating quadratics 2017
-var a, b, c, context, w, h, k=10;
+var a, b, c, context, w, h, k=18;
 
 function init() {
   canvas= document.getElementById("mycanvas");
@@ -7,6 +7,7 @@ function init() {
   w = canvas.width = 600;
   h = canvas.height = 400;
   console.log('canvas is loaded into context',w);
+  graphpaper();
 }  // close init
 
 function QF() {
@@ -14,15 +15,88 @@ function QF() {
   a = $("#quadA").val();
   b = $("#linB").val();
   c = $("#constant").val();
-  x1 = (-b+math.sqrt(b**2-4*a*b))/(2*a)
-  x2 = (-b-math.sqrt(b**2-4*a*b))/(2*a)
-  $("#solution1").text("Xintercept is at"+x1)
+  x1 = (-b+Math.sqrt(b**2-4*a*b))/(2*a);
+  x1 = Math.round (x1*10000)/ 10000;
+  x2 = (-b-Math.sqrt(b**2-4*a*b))/(2*a);
+  $("#solution1").text ("Xintercept is at "+x1);
+  $("#solution2").text ("Xintercept is at "+x1);
   console.log(a,b,c);
+  results();
+  graphQuad();
 }  // close QF
 
 function results() {
   // finding vertext and displaying symline and yint results
   vX = -(b*1)/(2*a);
   vY = a*Math.pow(vX,2)+b*vX+c*1;
-  $(".vertex").text("Vertex is at (" + vX+","+vY+")");
+  $("#vertex").text("Vertex is at (" + vX+","+vY+")");
+
 }  // close results() 
+
+function graphpaper() {
+  // the x and y axis drawn
+context.lineWidth= 3;
+context.beginPath();
+context.moveTo(w/2, 0);
+context.lineTo(w/2, h);
+context.stroke();
+
+context.beginPath();
+context.moveTo(0,h/2);
+context.lineTo(w,h/2);
+context.stroke();
+
+  // thin line with a 50% opacity using rgba() 
+  context.lineWidth=1;
+  context.strokeStyle="rgba(0,0,255,.5)";
+
+  //using the direct variation constant, k
+  //  here are the vertical and horizontal lines
+
+  for (i=0; i<h/(2*k); i++) {
+    context.beginPath();
+    context.moveTo( 0, h/2-i*k );
+    context.lineTo( w, h/2-i*k );
+    context.stroke();
+    context.beginPath();
+    context.moveTo( 0, h/2+i*k );
+    context.lineTo( w, h/2+i*k );
+    context.stroke();
+
+  }
+    // thin line with a 50% opacity using rgba() 
+  context.lineWidth=1;
+  context.strokeStyle="rgba(0,0,255,.5)";
+
+  //using the direct variation constant, k
+  //  here are the vertical and horizontal lines
+
+  for (i=0; i<w/(2*k); i++) {
+    context.beginPath();
+    context.moveTo( w/2-i*k, 0 );
+    context.lineTo( w/2-i*k, h );
+    context.stroke();
+    context.beginPath();
+    context.moveTo( w/2-i*-k, 0 );
+    context.lineTo( w/2-i*-k, h );
+    context.stroke();
+
+  }
+}  //close of graphpaper
+
+function graphQuad () {
+  for (var i = 0; i < w; i++) {
+    x = (w/2-i)/k;
+    y = c*1+b*x+a*Math.pow(x,2);
+    nx =  (w/2-(1+i))/k;
+    ny =  c*1+b*x+a*Math.pow(nx,2);
+    console.log (x,y,nx,ny);
+    context.beginPath();
+    context.lineWidth = 2;
+    context.strokeStyle = "Red";
+    context.moveTo(w/2+x*k,h/2-y*k);
+    context.lineTo(w/2+nx*k,h/2-ny*k);
+    context.stroke();
+  }
+}
+ 
