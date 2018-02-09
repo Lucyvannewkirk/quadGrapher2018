5e9000f9113eed4fdeaf5e0729f8176315aa04a8
Lucyvannewkirk <35277779+Lucyvannewkirk@users.noreply.github.com>
2018-02-09 12:39:29 -0800

    new new

--- quadScript.js
+++ quadScript.js
@@ -15,21 +15,59 @@ function QF() {
   a = $("#quadA").val();
   b = $("#linB").val();
   c = $("#constant").val();
-  x1 = (-b+Math.sqrt(b**2-4*a*b))/(2*a);
+  x1 = (-b+Math.sqrt(b**2-4*a*c))/(2*a);
   x1 = Math.round (x1*10000)/ 10000;
-  x2 = (-b-Math.sqrt(b**2-4*a*b))/(2*a);
-  $("#solution1").text ("Xintercept is at "+x1);
-  $("#solution2").text ("Xintercept is at "+x1);
+  x2 = (-b-Math.sqrt(b**2-4*a*c))/(2*a);
+  x2 = Math.round (x2 *10000) / 10000;
+
+   $("#solution1").text ("Xintercept is at "+x1);
+  $("#solution2").text ("Xintercept is at "+x2);
   console.log(a,b,c);
-  results();
   graphQuad();
+  results();
+  context.beginPath();
+  context.arc(w/2+x1*k,h/2,3,0,6.28);
+  context.fill(); 
+  context.beginPath();
+  context.arc(w/2+x2*k,h/2,3,0,6.28);
+  context.fill();
 }  // close QF
 
 function results() {
   // finding vertext and displaying symline and yint results
   vX = -(b*1)/(2*a);
   vY = a*Math.pow(vX,2)+b*vX+c*1;
+  vX = vX.toFixed(2);
+  vY = vY.toFixed(2);
   $("#vertex").text("Vertex is at (" + vX+","+vY+")");
+  context.beginPath();
+  context.arc(w/2+vX*k,h/2-vY*k,3,0,6.28);
+  context.fill();
+
+  $ ("#yint").text ("Y inercept is at (0,"+ c+")");
+  context.beginPath();
+  context.arc(w/2,h/2-c*k,3,0,6.28);
+  context.fill();
+
+
+  $("cp").text("Corr. Pt. is at ("+cp+","+ c+")");
+  context.beginPath();
+  context.arc(w/2+2*vX*k,h/2-c*k,3,0,6.28);
+  context.fill();
+
+  $("sline").text("Symmetry line is at x= "+ vX);
+
+
+
+   Xcp = 2*vX;
+
+   context.strokeStyle="0,50,200,.3";
+   context.setLineDash ([5,5]);
+   context.beginPath();
+   context.moveTo(w/2+vX*k,5);
+   context.lineTo(w/2+vX*k,h+5);
+   context.stroke();
+   context.setLineDash([0]);
 
 }  // close results() 
 
@@ -45,6 +83,7 @@ context.beginPath();
 context.moveTo(0,h/2);
 context.lineTo(w,h/2);
 context.stroke();
+
 
   // thin line with a 50% opacity using rgba() 
   context.lineWidth=1;
@@ -89,14 +128,31 @@ function graphQuad () {
     x = (w/2-i)/k;
     y = c*1+b*x+a*Math.pow(x,2);
     nx =  (w/2-(1+i))/k;
-    ny =  c*1+b*x+a*Math.pow(nx,2);
-    console.log (x,y,nx,ny);
+    ny =  c*1+b*nx+a*Math.pow(nx,2);
     context.beginPath();
     context.lineWidth = 2;
     context.strokeStyle = "Red";
     context.moveTo(w/2+x*k,h/2-y*k);
     context.lineTo(w/2+nx*k,h/2-ny*k);
     context.stroke();
-  }
 }
+}
+    function zoomIn () {
+      k=k+3;
+      init();
+      graphQuad();
+      results();
+      QF();
+    }
+
+      function zoomOut () {
+        k=k-3;
+      init();
+      graphQuad();
+      results();
+      QF();
+      }
+
+
+
  
